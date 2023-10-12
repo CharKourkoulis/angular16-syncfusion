1c4ec27de658d3269d6ed1c766ce2b7f3ef4480e
charKourkoulis <cult_bob@hotmail.com>
2023-10-12 15:52:51 +0300

    feat: datagrid component refactor

--- src/app/components/datagrid/datagrid.component.ts
+++ src/app/components/datagrid/datagrid.component.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from 'src/app/services/products.service';
 import { EditSettingsModel, FilterSettingsModel, GridModule, PageSettingsModel, ToolbarItems } from '@syncfusion/ej2-angular-grids';
@@ -17,12 +17,12 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './datagrid.component.html',
   styleUrls: ['./datagrid.component.css']
 })
-export class DatagridComponent implements OnInit {
+export class DatagridComponent {
   //syncfusion data grid related properties
-  public pageSettings?: PageSettingsModel;
-  public editSettings?: EditSettingsModel;
-  public toolbar?: ToolbarItems[];
-  public sortOptions?: object;
+  public pageSettings?: PageSettingsModel = { pageSize: 12, pageSizes: [10, 12, 20, 50] };
+  public editSettings?: EditSettingsModel  = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
+  public toolbar?: ToolbarItems[] = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+  public sortOptions?: object = { columns: [{ field: 'id', direction: 'Ascending' }] };
   public filterOption?: FilterSettingsModel = { type: 'Excel' };
   public height = '220px';
   public fields?: object = { text: 'id', value: 'id' };
@@ -31,13 +31,6 @@ export class DatagridComponent implements OnInit {
 
   constructor(private productsService: ProductsService){
 
-  }
-
-  ngOnInit(): void {
-    this.editSettings  = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
-    this.pageSettings = { pageSize: 12, pageSizes: [10, 12, 20, 50] };
-    this.toolbar = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
-    this.sortOptions = { columns: [{ field: 'id', direction: 'Ascending' }] };
   }
 
 }
